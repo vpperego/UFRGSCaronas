e14108bd5ec76e2dbabfee028711bdcd8a55c74c
RodrigoOkido <rodrigo_okido@hotmail.com>
2017-06-01 21:30:07 -0300

    Implementação de Interfaces, melhorias no design e logotipo

---
+++ app/src/main/java/com/example/user/ufrgscaronas/MainProgram.java
@@ -0,0 +1,23 @@
+package com.example.user.ufrgscaronas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainProgram extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToSearchRide(View view){
+        Intent intent = new Intent(this, SearchRide.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/user/ufrgscaronas/SearchRide.java
@@ -0,0 +1,35 @@
+package com.example.user.ufrgscaronas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SearchRide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_ride);
+
+        Spinner dropdownDeparture = (Spinner)findViewById(R.id.spinnerPartida);
+        String[] itemsDeparture = new String[]{"Campus Centro", "Campus Vale", "Campus ESEFID"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsDeparture);
+        dropdownDeparture.setAdapter(adapter);
+
+        Spinner dropdownArrival = (Spinner)findViewById(R.id.spinnerChegada);
+        String[] itemsArrival = new String[]{"Campus Centro", "Campus Vale", "Campus ESEFID"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsArrival);
+        dropdownArrival.setAdapter(adapter1);
+
+
+    }
+
+
+    public void checkRide(){
+
+
+
+
+    }
+}
